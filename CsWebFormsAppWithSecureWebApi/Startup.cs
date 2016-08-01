3703bc71ae26d09eb996551bd38ebe442eb40f75
Miroslaw Majka <miroslaw.majka@c2software.com>
2016-08-01 16:52:59 +0100

    Add project files.

---
+++ CsWebFormsAppWithSecureWebApi/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CsWebFormsAppWithSecureWebApi.Startup))]
+namespace CsWebFormsAppWithSecureWebApi
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
